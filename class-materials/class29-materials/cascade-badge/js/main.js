21437925006229ffba34d39689e3d76cfb0cfdc0
Tim Lines <timlines89@gmail.com>
2024-11-06 09:00:23 -0600

    6 November 9AM

--- class-materials/class29-materials/cascade-badge/js/main.js
+++ class-materials/class29-materials/cascade-badge/js/main.js
@@ -1,11 +1,36 @@
 //Arrays
 //Your pokemon party order which is a list of pokemon has been leaked to Misty. Please create a function that reverses your list and prints it to the console. 
 
-//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in a is strictly greater than the sum of the cubes of each element in b.
 
+// let myPokemon = ['Charmander', 'Squirtle', 'Bulbasaur', 'Butterfree', 'Garados', 'Jigglepuff'];
+
+// console.log(myPokemon);
+
+// function reversePokemon(arr) {
+//     return arr.reverse();
+// }
+
+// console.log(reversePokemon(myPokemon));
+
+
+
+//Given two integer arrays a, b, both of length >= 1, create a program that returns true if the sum of the squares of each element in 'a' is strictly greater than the sum of the cubes of each element in 'b'.
+
+
+function sumOfSquares( a, b){
+    return ( a.forEach((element) => element ** 2) > b.forEach((element) => element ** 3) )
+}
+
+console.log(sumOfSquares( [4], [2] ));
 //Return a new array consisting of elements which are multiple of their own index in input array (length > 1).
 // Some cases:
 // [22, -6, 32, 82, 9, 25] =>  [-6, 32, 25]
 // [68, -1, 1, -7, 10, 10] => [-1, 10]
 
+
+function returnArray(){
+    (length > 1)
+}
+
 //Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.Return your answer as a number.
+
---
+++ other-projects/csharp/HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      //Your code here
+      //This variable will be used to count the number of tiems the word travel is used in a document
+      int travelCount = 0;
+
+      /* Calcualtes
+    }
+  }
+}
