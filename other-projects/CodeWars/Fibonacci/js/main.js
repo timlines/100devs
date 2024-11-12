00dac8abc44c08206bd80c2e2a82b6d9a6abf946
Tim Lines <timlines89@gmail.com>
2024-11-12 17:57:47 -0600

    11 Nov

--- other-projects/CodeWars/Fibonacci/js/main.js
+++ other-projects/CodeWars/Fibonacci/js/main.js
@@ -89,24 +89,24 @@
 // countDown(3);
 
 
-function fib(n){
+// function fib(n){
 
-    if(n <= 1){
-        return n;
-    }
-    else{
+//     if(n <= 1){
+//         return n;
+//     }
+//     else{
 
-       return fib(n - 1) + fib(n - 2) ;
-    }
-}
+//        return fib(n - 1) + fib(n - 2) ;
+//     }
+// }
 
-console.log('fib(0) -> 0 : ' + fib(0));
-console.log('fib(1) -> 1 : ' + fib(1));
-console.log('fib(2) -> 1 : ' + fib(2));
-console.log('fib(3) -> 2 : ' + fib(3));
-console.log('fib(4) -> 3 : ' + fib(4));
-console.log('fib(5) -> 5 : ' + fib(5));
-console.log('fib(6) -> 8 : ' + fib(6));
+// console.log('fib(0) -> 0 : ' + fib(0));
+// console.log('fib(1) -> 1 : ' + fib(1));
+// console.log('fib(2) -> 1 : ' + fib(2));
+// console.log('fib(3) -> 2 : ' + fib(3));
+// console.log('fib(4) -> 3 : ' + fib(4));
+// console.log('fib(5) -> 5 : ' + fib(5));
+// console.log('fib(6) -> 8 : ' + fib(6));
 
 // 0,1,1,2,3,5,8,13,21,34
 // fib(0) -> 0
@@ -118,3 +118,57 @@ console.log('fib(6) -> 8 : ' + fib(6));
 // fib(6) -> 8
 
 //f(n - 1) + f(n - 2), for n > 1
+
+
+
+
+// Attempt 2
+
+// The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+// F(0) = 0, F(1) = 1
+// F(n) = F(n - 1) + F(n - 2), for n > 1.
+// Given n, calculate F(n).
+
+// Example 1:
+
+// Input: n = 2
+// Output: 1
+// Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+// Example 2:
+
+// Input: n = 3
+// Output: 2
+// Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+// Example 3:
+
+// Input: n = 4
+// Output: 3
+// Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+
+function fib(num){
+    if (num < 2){
+        return num;
+    }else{
+        return fib(num - 1 ) + fib(num - 2);
+    }
+}
+
+console.log(fib(1));
+
+// memoiazation
+
+const memo = {}
+
+function fib(n) {
+    if(n < 2){
+        return n;
+    }else if ( memo[n]){
+        return memo[n]
+    }else{
+        let result = fib(n - 1) + fib(n - 2)
+        memo[n] = result
+        return result
+    }
+}
+
--- other-projects/csharp/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ other-projects/csharp/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21437925006229ffba34d39689e3d76cfb0cfdc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35ba01c37e1c9b7dec49b4765a126bb91fff3a0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
