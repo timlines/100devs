d1a0c87f611f65fd11bab0a0d2fe5252ef96d97c
Tim Lines <timlines89@gmail.com>
2024-11-17 13:23:43 -0600

    17 Nov 123 PM

--- other-projects/CodeWars/0 working/js/main.js
+++ other-projects/CodeWars/0 working/js/main.js
@@ -1,5 +1,18 @@
-function getCount(str){
-    return str.match(/[aeiou]/ig).length
+function guessBlue(blueStart, redStart, bluePulled, redPulled){
+    console.log(`Number of Blue Marbles: ${blueStart}`);
+    console.log(`Number of Red Marbles: ${redStart}`);
+    console.log(`Number of Red Pulled ${bluePulled}`);
+    console.log(`Number of Blue Pulled ${redPulled}`);
+
+    let probBlue = 0;
+
+    probBlue = (blueStart - bluePulled) / ((blueStart - bluePulled) + (redStart - redPulled));
+    
+    
+    
+    console.log(probBlue);
+    return probBlue;
 }
 
-console.log(getCount('abcdefg')); // 2
+
+guessBlue(5, 5, 2, 3); //0.6
