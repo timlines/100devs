5e5b98922d6153487b8db2d1449d351a79f86225
Tim Lines <timlines89@gmail.com>
2024-11-30 17:44:06 -0600

    30 Nov 545 PM

--- class-materials/class29-materials/boulder-badge/js/main.js
+++ class-materials/class29-materials/boulder-badge/js/main.js
@@ -6,7 +6,7 @@ let caterpie = 7 - 1
 let weedle = 7 - 1
 let totalCandies = bulbasaur + caterpie + weedle;
 
-console.log(bulbasaur, caterpie, weedle, totalCandies)
+console.log(`Bulbasaur: ${bulbasaur}\nCaterpie: ${caterpie}\nWeedle: ${weedle}\nTotal Candies: ${totalCandies}`)
 
 //Conditionals And Functions
 //You have a charmander in your party. Charmander can only battle if the temperature is above 0 degrees celcius. Create one function that converts a Fahrenheit value to Celcius and another fuction that tells you wheither or not charmander can battle
@@ -16,21 +16,40 @@ function fahrenheitToCelcius(fahrenheit){
     return celcius;
 }
 
+// function fahrenheitToCelcius(fahrenheit){
+//     let celcius = (fahrenheit - 32_ * 5/9;
+//     return celcius;
+//     )
+// }
+
+console.log(fahrenheitToCelcius(20));
+
 function battleCharmander(fahrenheit){
     return fahrenheitToCelcius(fahrenheit) > 0;
 
 }
 
-console.log(battleCharmander(20));
+console.log(battleCharmander(33));
 
 //Loops
 //You have joined an undeground pokemon leauge. In this league, trainers can use any number of pokemon. Print to the console "Pikachu I choose you" x times where x is the number of pokemon the trainer you are battling has in their party
 
-
-let numberOfPokemon = 10;
-
-while( numberOfPokemon > 0){
-    console.log("Pikachu I choose you")
-    numberOfPokemon --;
+function iChooseYou(x){
+    
+    for ( let i = 0; i < x; i++){
+        console.log('Pikachu I choose you!');
+    }
 }
+
+iChooseYou(10);
+
+
+
+
+// let numberOfPokemon = 10;
+
+// while( numberOfPokemon > 0){
+//     console.log("Pikachu I choose you")
+//     numberOfPokemon --;
+// }
 
--- other-projects/csharp/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ other-projects/csharp/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ca403007e35c6b7fc609d3237bb1f5bf1c12649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14df6f4efd9ac28e43ad23c7a7f2698dfc588544")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
