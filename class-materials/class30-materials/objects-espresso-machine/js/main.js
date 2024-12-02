464ad0c1b1930cbe34d7ed6d11805b22b91c56ee
Tim Lines <timlines89@gmail.com>
2024-12-02 17:35:10 -0600

    2 Dec 535 PM

--- class-materials/class30-materials/objects-espresso-machine/js/main.js
+++ class-materials/class30-materials/objects-espresso-machine/js/main.js
@@ -27,3 +27,70 @@ let gaggia = new EspressoMachine('red', 'Gaggia', 'Classic Pro', 400 )
 
 
 
+// let hourlyRate =250;
+// let hours = 160
+// let taxRate = .35
+
+// function calculateProfit(rate, numOfHours, taxes){
+//     return rate * numOfHours * (1 - taxes)
+// }
+
+// function holdForTaxes(profitMade){
+//     return hourlyRate * hours - profitMade
+// }
+
+// let profit = calculateProfit(hourlyRate, hours, taxRate)
+
+// let taxesHeld = holdForTaxes(profit)
+
+// console.log(profit);
+// console.log(taxesHeld)
+
+
+let seriousBusinessPerson = {
+    hourlyRate: 250,
+    hours: 160,
+    taxRate: .35,
+    calculateProfit: function() {
+        return this.hourlyRate * this.hours * (1 - this.taxRate)
+    }
+    calculateTaxesHeld: function() {
+        return this.hourlyRate * this.hours - this.calculateProfit()
+    }
+}
+
+
+function AgencyContractor(hourlyRate, hours, taxRate){
+    this.hourlyRate = hourlyRate
+    this.hours = hours
+    this.taxRate = taxRate
+    this.calculateProfit = function() {
+        return this.hourlyRate + thishours * (1 - this.taxRate)
+    }
+    this.invoiceClient = function(){
+        return `Your invoice total is ${this.hourlyRate * this.hours}`
+    }
+
+    let leon = new AgencyContractor(250,160,.35)
+    console.log( leon.invoiceClient() )
+    console.log( leon.hourlyRate )
+    console.log( leon.calculateProfit() )
+}
+
+function AgencyContractor(hourlyRate, hours, taxRate){
+    this.hourlyRate = hourlyRate
+    this.hours = hours
+    this.taxRate = taxRate
+    this.calculateProfit = function() {
+        return this.hourlyRate + thishours * (1 - this.taxRate)
+    }
+    this.invoiceClient = function(){
+        return `Your invoice total is ${this.hourlyRate * this.hours}`
+    }
+
+    let leon = new AgencyContractor(250,160,.35)
+    console.log( leon.invoiceClient() )
+    console.log( leon.hourlyRate )
+    console.log( leon.calculateProfit() )
+}
+
