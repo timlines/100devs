d894b78a0da919901f9ec69ddca2ee74f7f50d3a
Tim Lines <timlines89@gmail.com>
2024-12-04 15:26:39 -0600

    createPhoneNumber

--- other-projects/CodeWars/createPhoneNumber/js/main.js
+++ other-projects/CodeWars/createPhoneNumber/js/main.js
@@ -6,6 +6,18 @@
 
 // Don't forget the space after the closing parentheses!
 
+// function createPhoneNumber(numbers){
+//     return `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[8]}`
+// }
+
 function createPhoneNumber(numbers){
-    
+    let format = '(xxx) xxx-xxxx';
+
+    for(let i = 0; i < numbers.length; i++){
+        format = format.replace('x', numbers[i]);
+    }
+
+    return format;
 }
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])) // "(123) 456-7890"
