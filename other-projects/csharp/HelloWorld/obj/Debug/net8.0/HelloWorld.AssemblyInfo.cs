a1dadba6a5fcb77426df0ebdd6f1ac1c61ac9be1
Tim Lines <timlines89@gmail.com>
2024-12-08 21:51:46 -0600

    8 Dec 951 PM

--- other-projects/csharp/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ other-projects/csharp/HelloWorld/obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fcbd3d425177b442e2a9e0be1c196e8e089d09d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+301ed6ff0d5d31ea8448dc188aceb6cada069d15")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
