3704dfd21fcf4ea769f2cd7bf6fa120f98c334f6
Tim Lines <timlines89@gmail.com>
2024-12-12 13:54:37 -0600

    miriam

--- class-materials/class32-materials/asynchoronousJavascript/js/main.js
+++ class-materials/class32-materials/asynchoronousJavascript/js/main.js
@@ -1,4 +1,13 @@
 
+function makeGreeting(name) {
+    return `Hello, my name is ${name}!`;
+}
+
+
+
 const name = 'Miriam';
-const greeting = `Hello, my name is ${name}!`;
+const greeting = makeGreeting(name);
 console.log(greeting);
+
+// Hello, my name is Miriam!
+
