1faa930ebe34fd8818329c4ca76dcf90eeb93ca3
Tim Lines <timlines89@gmail.com>
2024-12-13 16:53:33 -0600

    public folder

--- class-materials/class37-materials/nodejsCrashCourse/index.js
+++ class-materials/class37-materials/nodejsCrashCourse/index.js
@@ -1,6 +1,15 @@
-const Person = require('./person');
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+    if( req.url === '/'){
+        res.writeHead(200, { 'Content-Type': 'text.html'})
+        res.end('<h1>Home</h1>');
 
-const person1 = new Person('John Doe', 30);
+    }
+});
 
-person1.greeting();
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- class-materials/class37-materials/nodejsCrashCourse/logger.js
+++ class-materials/class37-materials/nodejsCrashCourse/logger.js
@@ -1,6 +1,17 @@
 const EventEmitter = require('events');
 const uuid = require('uuid');
 
-console.log(uuid.v4());
-console.log(uuid.v4());
-console.log(uuid.v4());
+class Logger extends EventEmitter {
+    log(msg) {
+        // Call event
+        this.emit('message', { id: uuid.v4(), msg });
+    }
+}
+
+// module.exports = Logger;
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log('Called Listener', data));
+
+logger.log('hello world');
