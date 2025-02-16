8c4097a7787ad42ac6119ea8e98b7dd8e25328ec
Tim Lines <timlines89@gmail.com>
2025-02-15 20:31:32 -0600

    831 update

--- appwrite/hitlistDemo/src/main.js
+++ appwrite/hitlistDemo/src/main.js
@@ -1,31 +1,87 @@
 import './style.css'
+import { PROJECT_ID, DATABASE_ID, COLLECTION_ID } from './shhh.js';
 
 import { Client, Databases, ID } from "appwrite";
 
 const client = new Client()
     .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('67afce6c003b33821b8a');
+    .setProject(PROJECT_ID);
 
 const databases = new Databases(client);
 
-const promise = databases.createDocument(
-    '67afcf140006ef072b62',
-    '67afcf240038ceaf621a',
+const form = document.querySelector('form');
+
+form.addEventListener('submit', addJob)
+
+function addJob(e){
+  e.preventDefault();
+  const job = databases.createDocument(
+    DATABASE_ID,
+    COLLECTION_ID,
     ID.unique(),
-    { "company-name": "100Devs",
-      "date-added": new Date(),
-      "role": "software engineer",
-      "location": "Philly",
-      "position-type": "full time",
-      "source": "https://100devs.org"
-     }
+    { "company-name": e.target.companyName.value,
+      "date-added": e.target.dateAdded.value,
+      "role": e.target.role.value,
+      "location": e.target.location.value,
+      "position-type": e.target.positionType.value,
+      "source": e.target.source.value
+    }
+  );
+  
+  job.then(function (response) {
+    addJobsToDom();
+  }, function (error) {
+    console.log(error);
+  });
+  form.reset()
+
+}
+
+
+async function addJobsToDom(){
+  let response = await databases.listDocuments(
+    DATABASE_ID,
+    COLLECTION_ID,
+
 );
 
-promise.then(function (response) {
-    console.log(response);
-}, function (error) {
-    console.log(error);
-});
+// console.log(response.documents[0]);
+
+response.documents.forEach((job)=> {
+
+  const li = document.createElement('li');
+  li.textContent = `${job['company-name']} ${job['date-added']} ${job['role']} ${job['location']} ${job['position-type']} ${job['source']}`
+  document.querySelector('ul').appendChild(li);
+ 
+})
+
+
+// response.then(function (response) {
+//     console.log(response);
+// }, function (error) {
+//     console.log(error);
+// });
+
+}
+
+addJobsToDom();
+
+
+
+// const promise = databases.createDocument(
+//     DATABASE_ID,
+//     COLLECTION_ID,
+//     ID.unique(),
+//     { "company-name": "100Devs",
+//       "date-added": new Date(),
+//       "role": "software engineer",
+//       "location": "Philly",
+//       "position-type": "full time",
+//       "source": "https://100devs.org"
+//      }
+// );
+
+
 
 
 
